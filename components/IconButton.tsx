8ddcca32393d37b0f34af8a68acdef695cbc4a25
alok-karn <alokkyasth@gmail.com>
2023-11-14 11:28:52 +0530

    fix

--- components/IconButton.tsx
+++ components/IconButton.tsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import { View, TouchableOpacity, Text } from 'react-native';
+import React from "react";
+import { View, TouchableOpacity, Text } from "react-native";
 
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import Icon from "react-native-vector-icons/MaterialIcons";
 
 const IconButton = ({ onPress, iconName, color }) => {
-  return (
-    <TouchableOpacity onPress={onPress}>
-      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <Icon name={iconName} size={30} color={color} />
-        
-      </View>
-    </TouchableOpacity>
-  );
+    return (
+        <TouchableOpacity onPress={onPress}>
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+                <Icon name={iconName} size={30} color={color} />
+            </View>
+        </TouchableOpacity>
+    );
 };
 
 export default IconButton;
